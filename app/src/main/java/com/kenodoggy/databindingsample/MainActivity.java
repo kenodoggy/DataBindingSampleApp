2a5b6f2e34964fe24ea2a0cc265a8643c64ed2aa
brenda cook <bcook@sportslabs.com>
2015-09-09 19:58:29 -0600

    Initial commit

---
+++ app/src/main/java/com/kenodoggy/databindingsample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kenodoggy.databindingsample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.kenodoggy.databindingsample.databinding.ActivityMainBinding;
+import com.kenodoggy.databindingsample.observable.BindableObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BindableObject bindableObject;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bindableObject = new BindableObject();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setObj(bindableObject);
+    }
+
+    public void onClickReset(View v) {
+        bindableObject.setTextValue("");
+        bindableObject.setEditTextValue("");
+    }
+
+
+}
---
+++ app/src/main/java/com/kenodoggy/databindingsample/observable/BindableObject.java
@@ -0,0 +1,69 @@
+package com.kenodoggy.databindingsample.observable;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
+
+
+public class BindableObject extends BaseObservable{
+
+    private String textValue;
+    private String editTextValue;
+
+    @Bindable
+    public String getTextValue() {
+        return textValue != null ? textValue : "";
+    }
+
+    public void setTextValue(String value) {
+        if (verifyNewStringIsValid(textValue, value)) {
+            this.textValue = value;
+            notifyChange();
+        }
+    }
+
+    @Bindable
+    public String getEditTextValue() {
+        return editTextValue != null ? editTextValue : "";
+    }
+
+    public void setEditTextValue(String value) {
+        if (verifyNewStringIsValid(editTextValue, value)) {
+            this.editTextValue = value;
+            notifyChange();
+        }
+    }
+
+    private boolean verifyNewStringIsValid(String oldValue, String newValue) {
+        boolean isDifferent = false;
+        if (oldValue == null && newValue != null) {
+            isDifferent = true;
+        }
+
+        else if (oldValue != null && newValue != null && !oldValue.equals(newValue)) {
+            isDifferent = true;
+        }
+        return isDifferent;
+    }
+
+    public TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            setEditTextValue(s.toString());
+            setTextValue(getEditTextValue());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Selection.setSelection(s, s.length());
+        }
+    };
+
+
+}
